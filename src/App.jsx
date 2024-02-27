859f384155d6399431c596f5ee5607f149150296
MD Enjamul Islam <mdenjamulislam527@gmail.com>
2024-02-27 15:39:21 +0600

    Text AI response successfully showed

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import AiWithImage from "./components/AiWithImage";
+import AiWithText from "./components/AiWithText";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <h2 className="text-2xl font-semibold text-center">Today I build a AI using google ai key.</h2>
-            <AiWithImage />
+            <AiWithText/>
         </>
     );
 }
---
+++ src/components/AiWithText.jsx
@@ -0,0 +1,56 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { useState } from "react";
+
+const AiWithText = () => {
+    const genAi = new GoogleGenerativeAI("AIzaSyD6vHdHfkcWN-NfLZSzCz2jZBBMsU4Xkcg");
+
+    const [search, setSearch] = useState("");
+    const [aiResponse, setAiResponse] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    async function aiRun() {
+        setLoading(true);
+        setAiResponse("");
+        const model = genAi.getGenerativeModel({ model: "gemini-pro" });
+        const prompt = `${search}`;
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+        setAiResponse(text);
+        setLoading(false);
+    }
+
+    // search
+    const handleChangeSearch = (e) => {
+        setSearch(e.target.value);
+        
+    };
+    // submit button
+    const handleClick = () => {
+        aiRun();
+    };
+
+    return (
+        <section className="relative">
+            <div className="container mx-auto h-screen relative">
+                {/* Result */}
+                <div className="w-full ">
+                    <h2 className="text-xl md:text-2xl font-semibold text-center pt-10 py-5">Google Gemini AI Copy</h2>
+                    <div className="p-5 border border-gray-300 rounded-xl space-y-5">
+                        <p className="font-medium">AI Response Here</p>
+                        {loading == true && aiResponse == "" ? <span className="loading loading-dots loading-lg"></span> : <p className="text-sm md:text-base">{aiResponse}</p>}
+                    </div>
+                </div>
+                {/* Prompt Field */}
+                <div className="flex items-center gap-3 absolute bottom-10 left-0 right-0">
+                    <input type="text" placeholder="Type here" onChange={(e) => handleChangeSearch(e)} className="w-full px-7 py-2.5 border border-gray-400 rounded-xl" />
+                    <button onClick={() => handleClick()} className="px-6 py-2.5 bg-slate-700 text-white rounded-xl">
+                        Generate
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AiWithText;
