f205d661e09d6002dfe52c8347e04d5ee09086db
MD Enjamul Islam <mdenjamulislam527@gmail.com>
2024-02-29 16:19:06 +0600

    Header nav design

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './layouts/Home.jsx'
-import Root from './layouts/Root.jsx'
-import AiWithText from './components/AiWithText.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./layouts/Home.jsx";
+import Root from "./layouts/Root.jsx";
+import AiWithText from "./components/AiWithText.jsx";
+import Login from "./layouts/Login.jsx";
+import Register from "./layouts/Register.jsx";
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root/>,
-    children: [
-      {
-        path: '/',
-        element: <Home/>
-      },
-      {
-        path: '/text-ai',
-        element: <AiWithText/>
-      }
-    ]
-  }
-])
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                path: "/home",
+                element: <Home />,
+            },
+            {
+                path: "/text-ai",
+                element: <AiWithText />,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+        ],
+    },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router}></RouterProvider>
+    </React.StrictMode>
+);
